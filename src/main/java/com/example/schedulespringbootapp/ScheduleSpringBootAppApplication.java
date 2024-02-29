53f3c3e7cacc8cf72f88b766cdf0e2f1b8492803
Szer13 <shad350@gmail.com>
2024-02-29 15:05:32 +0100

    init commit

---
+++ src/main/java/com/example/schedulespringbootapp/ScheduleSpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.schedulespringbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleSpringBootAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/schedulespringbootapp/ScheduleSpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schedulespringbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleSpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
