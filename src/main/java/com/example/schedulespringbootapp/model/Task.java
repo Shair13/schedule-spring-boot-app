6538aa0ffdbfc1a7a81c6477e467015d5889a14f
Szer13 <shad350@gmail.com>
2024-02-29 21:25:33 +0100

    model, repo, validator

---
+++ src/main/java/com/example/schedulespringbootapp/model/Task.java
@@ -0,0 +1,22 @@
+package com.example.schedulespringbootapp.model;
+
+import jakarta.persistence.*;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @NotBlank(message = "Task's description cannot be empty")
+    private String description;
+    private boolean done;
+}
